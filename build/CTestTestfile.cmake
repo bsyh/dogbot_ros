1e69fd322d79186724fe037aabd44a1eef267b78
vboxuser <vboxuser@ros.myguest.virtualbox.org>
2022-11-11 23:08:10 -0500

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/vboxuser/sanctuary_demo/src
+# Build directory: /home/vboxuser/sanctuary_demo/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("dogbot")
---
+++ build/dogbot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vboxuser/sanctuary_demo/src/dogbot
+# Build directory: /home/vboxuser/sanctuary_demo/build/dogbot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
